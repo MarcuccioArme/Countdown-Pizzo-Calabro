b880fad5910b0ed3d1cebe423ade9610b278a03b
1lgrand <93736234+1lgrand@users.noreply.github.com>
2023-05-03 19:44:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var x = setInterval(function() {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("countdown").innerHTML = days + "g " + hours + "o "
+    document.getElementById("countdown").innerHTML = days + "g " + hours + "h "
     + minutes + "m " + seconds + "s ";
         if (distance < 0) {
             clearInterval(x);
